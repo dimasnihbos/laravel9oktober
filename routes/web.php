ce12c82d5edcbcd5dfd6411d019b5684f61acc95
dimasnihbos <90371402+dimasnihbos@users.noreply.github.com>
2023-10-09 09:37:13 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\dimasController;
+use App\Http\Controllers\masyarakatController;
+use App\Http\Controllers\petugasController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
+
+Route::get('/home', [dimasController::class, 'index']);
+
+Route::get('isi', function(){
+  return view('isi');
+});
+
+Route::post('/isi', [dimasController::class, 'isi_pengaduan']);
+
+Route::get('masyarakat', [masyarakatController::class, 'rakyat']);
+
+Route::get('petugas', [petugasController::class, 'petugas']);
+
+Route::get('loginmasyarakat', [masyarakatController::class, 'Login']);
+
+Route::get('regis', [masyarakatController::class, 'register']);
+
+Route::get('logpetugas', [petugasController::class, 'loginpetugas']);
+
+Route::get('/hapus-pengaduan/{id}', [dimasController::class, 'hapus']);
+
+Route::get('/detail-pengaduan/{id}', [dimasController::class, 'detail_pengaduan']);
+
+//Route::get('/isi', function () {
+  //  return view('isi');
+//});
+
+//Route::get('/login', function () {
+  //  return view('login');
+//});
+
